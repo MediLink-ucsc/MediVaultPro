a2f536c70cb09957cecda9ca621d4e76625326ac
liki-ch <likithachathu@gmail.com>
2025-07-08 20:44:35 +0530

    notification UI added

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,6 +1,7 @@
 // src/components/Layout/Header.js
 import React from 'react';
-import { Bell, Search, LogOut, User } from 'lucide-react';
+import { Search, LogOut, User } from 'lucide-react';
+import Notification from './Notification';
 
 const Header = ({ user, onLogout }) => {
   return (
@@ -18,10 +19,7 @@ const Header = ({ user, onLogout }) => {
         </div>
 
         <div className="flex items-center space-x-4">
-          <button className="relative p-2 text-gray-600 hover:text-gray-800">
-            <Bell className="w-6 h-6" />
-            <span className="absolute top-0 right-0 w-3 h-3 bg-orange-500 rounded-full"></span>
-          </button>
+          <Notification />
 
           <div className="flex items-center space-x-3">
             <div className="w-8 h-8 bg-teal-600 rounded-full flex items-center justify-center">
