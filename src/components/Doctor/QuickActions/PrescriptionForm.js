9162df6fcd55fc0121c2366b2e3a9dd57137ef18
SathyaAbeytunge <sathyaabeytunge@gmail.com>
2025-08-09 18:24:26 +0530

    prescriptionForm updates

--- src/components/Doctor/QuickActions/PrescriptionForm.js
+++ src/components/Doctor/QuickActions/PrescriptionForm.js
@@ -39,10 +39,12 @@ const PrescriptionForm = ({ onSubmit, selectedPatient }) => {
 
 
     try {
+      const token = localStorage.getItem('token'); 
       const response = await fetch('http://localhost:3000/api/v1/patientRecords/prescriptions/insert', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
         },
         body: JSON.stringify(payload),
       });
