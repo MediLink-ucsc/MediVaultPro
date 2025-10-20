800b7eb579385419c8b00e479a7ce8ff9524ce39
Likitha Chathubhashini <likithachathu@gmail.com>
2025-10-20 11:08:29 +0530

    searchbar removed from header nav

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -19,16 +19,7 @@ const Header = ({ user, onLogout }) => {
     <>
       <header className="bg-white border-b border-gray-200 px-6 py-4">
         <div className="flex items-center justify-between">
-          <div className="flex items-center space-x-4">
-            <div className="relative">
-              <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400" />
-              <input
-                type="text"
-                placeholder="Search patients, calendar events..."
-                className="pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-teal-500 focus:border-transparent w-96"
-              />
-            </div>
-          </div>
+          <div></div>
 
           <div className="flex items-center space-x-4">
             <Notification />
